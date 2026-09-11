a2976b916a53a0faf6e6ef354e0e4e81c5e24607
Henry Vega <hp.vega@hotmail.com>
2026-09-10 20:12:25 -0500

    Add ciclo.py with a for loop example iterating over a list of fruits

---
+++ sesion-2/ejemplo/ciclo.py
@@ -0,0 +1,15 @@
+# ciclos con python
+
+# ciclo for -> Recorre cada elemento de una lista
+
+frutas = ["fresa", "limón", "piña", "mango", "sandia"]
+
+# for -> indica que vamos a repetir algo
+# variable fruta -> es una variable temporal que irá tomando cada elemento
+# in -> significa "dentro de"
+# frutas -> es nuestra lista
+# : -> indica que empieza el bloque de instrucciones
+# print -> es una función que imprime en pantalla
+
+for fruta in frutas:
+    print(f"Nombre de la fruta: {fruta}")
