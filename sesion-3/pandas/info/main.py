f32ecedff098f88de9af23e8d4161e08be3e7d08
Henry Vega <hp.vega@hotmail.com>
2026-09-15 21:59:49 -0500

    Add data_ventas_enero_2026.csv and main.py for sales data processing with pandas

---
+++ sesion-3/pandas/info/main.py
@@ -0,0 +1,12 @@
+import pandas as pd
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent
+DATA_DIR = BASE_DIR / "../../database"
+
+csv_path = DATA_DIR / "data_ventas_enero_2026.csv"
+
+# Leer el archivo CSV
+df = pd.read_csv(csv_path)
+
+print(df.info())  # Muestra información sobre el DataFrame
