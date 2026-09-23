3f5993d70f24acc5e94bbee75b411f987580945b
Henry Vega <hp.vega@hotmail.com>
2026-09-22 20:26:16 -0500

    Add main.py for reading dataset.txt and renaming 'Store ID' column to 'Codigo de tienda'

---
+++ sesion-5/pandas/renombrear_columna/main.py
@@ -0,0 +1,14 @@
+import pandas as pd
+from pathlib import Path
+
+DATA_DIR = (Path(__file__)).resolve().parent / "../../database"
+
+csv_data = DATA_DIR / "dataset.txt"
+
+# Leer el archivo CSV y crear un DataFrame
+df = pd.read_csv(csv_data)
+
+# Seleccionar una columna específica para renombrar
+df.rename(columns={"Store ID": "Codigo de tienda"}, inplace=True)
+
+print(df)
